8274a8701fa0747ae14666e791d754d7b275fce1
Dan Kimberley <danielkimberley@gmail.com>
2024-03-24 12:08:11 +0000

    functional calculator

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+let numberOne = 0
+let numberTwo = 0
+let operatorSelected = false
+let operator = ''
+
 // add event listener to all buttons on page, call buttonPressed when clicked
 const buttons = document.querySelectorAll('button')
 buttons.forEach((button) => {
@@ -15,7 +20,9 @@ function buttonPressed(input) {
     if (input.classList.contains('num')){
         numberButtonPressed(input)
     } else if (input.textContent == 'AC') {
-        output.textContent = 0
+        acButtonPressed()
+    } else if (input.classList.contains('op')) {
+        processOperator(input)
     }
 }
 
@@ -26,8 +33,50 @@ function numberButtonPressed(input) {
     }
     if (output.textContent == 0) {
         output.textContent = (input.textContent)
-        console.log('helloi')
     } else {
         output.textContent += (input.textContent)
     }
+}
+
+function acButtonPressed() {
+    output.textContent = 0
+    numberOne = 0
+    numberTwo = 0
+    operatorSelected = false
+    operator = ''
+}
+
+function processOperator(input) {
+    console.log(`Processing input ${input}`)
+    if (input.textContent == '=') {
+        numberTwo = output.textContent
+        calculate()
+    } else {
+        numberOne = output.textContent
+        operatorSelected = true
+        operator = input.textContent
+        output.textContent = 0
+    }
+}
+
+function calculate() {
+    let result = 0
+    numberOne = Number(numberOne)
+    numberTwo = Number(numberTwo)
+    console.log(numberOne)
+    console.log(numberTwo)
+    console.log(operator)
+    switch (operator) {
+        case "+": 
+            result = numberOne + numberTwo
+            break
+        case "-": 
+            result = numberOne - numberTwo
+            break
+        case "×": 
+            result = numberOne * numberTwo
+            break
+        case "÷": result = numberOne / numberTwo
+    }
+    output.textContent = result
 }
